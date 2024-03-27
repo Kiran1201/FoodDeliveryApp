f3498ae5cabe29f5b248e43f9c93bc141a166a38
Kiran1201 <kvkirankumar1201@gmail.com>
2024-03-27 16:44:43 +0530

    Food Delivery App

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swaggerSpec');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
