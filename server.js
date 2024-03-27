ee6851887842f3c98bd3aaac57be7c46da79248b
Kiran1201 <kvkirankumar1201@gmail.com>
2024-03-27 17:12:19 +0530

    Food Delivery App

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-const express = require('express');
-const appRoutes = require('./src/FoodDeliveryApp/routes');
-const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./swaggerSpec');
-
-const app = express();
+const http = require('http');
+const app = require('./app');
 const port = process.env.PORT || 3000;
-app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+const server = http.createServer(app);
 
-app.use("/api/v1/fooddeliveryapp", appRoutes);
-
-app.listen(port, () => console.log(`app listening on the  ${port}`));
+server.listen(port, () => {
+    console.log(`started on port ${port}`);
+})
